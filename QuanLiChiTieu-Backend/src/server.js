4b7f400dbdb6198da7b85abee63fa0e4efb297fb
DinhCongHieu_FIVECAT_22CT113 <163360363+DinhCongHieuFIVECAT22CT113@users.noreply.github.com>
2025-03-29 23:59:55 +0700

    Xử lý kết nối NodeJS với MongoDB

---
+++ QuanLiChiTieu-Backend/src/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("../config/db");
+
+// Kết nối MongoDB trước khi chạy server
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Route mặc định
+app.get("/", (req, res) => {
+    res.send("API QuanLiChiTieu đang chạy...");
+});
+
+// Khởi chạy server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`🚀 Server đang chạy tại http://localhost:${PORT}`);
+});
