4ea08cea34e4feffefbde4cac9d5ea86973a6d49
HuuPhuoc <phuoczmn@gmail.com>
2025-04-01 21:37:11 +0700

    API delete expense by id

--- QuanLiChiTieu-Backend/controllers/expensesController.js
+++ QuanLiChiTieu-Backend/controllers/expensesController.js
@@ -31,3 +31,18 @@ exports.updateExpenseById = async (req, res) => {
     res.status(500).json({ message: "Error updating expense", error: error.message });
   }
 };
+// Xóa expense theo ID
+exports.deleteExpenseById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedExpense = await Expense.findByIdAndDelete(id);
+
+    if (!deletedExpense) {
+      return res.status(404).json({ message: "Expense not found" });
+    }
+
+    res.status(200).json({ message: "Expense deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting expense", error: error.message });
+  }
+};
--- QuanLiChiTieu-Backend/routes/expensesRoutes.js
+++ QuanLiChiTieu-Backend/routes/expensesRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { filterExpensesByName ,updateExpenseById } = require("../controllers/expenseController");
+const { filterExpensesByName ,updateExpenseById,deleteExpenseById} = require("../controllers/expenseController");
 
 const router = express.Router();
 
 router.get("/expenses", filterExpensesByName);
 router.get("/expenses", updateExpenseById);
+router.get("/expenses",deleteExpenseById);
 module.exports = router;
